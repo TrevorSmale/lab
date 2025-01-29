83a9c6dc14f2e725a77a7f2f8ac425c496747ce4
Trevor Smale <trevorsmale@ts.local>
2025-01-29 09:02:21 -0500

    go fmt

---
+++ Go/Snippets/fmt/println1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("👷🏼‍♂️🪿⭐️")
+	fmt.Println(`
+	Printing a raw string literal using single quotation marks. 
+	No need for escape characters. 
+	Go will automatically do line wrapping. 
+	Also Unicode is baked in 🐍👽🏋🏼‍♂️⛑️🎯 like that.
+	`)
+}
